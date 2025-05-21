6824c1695fc97ca47b564b5d086eef619923facc
Kevin Sayers <ktsayers@amazon.com>
2025-05-20 21:46:42 -0600

    fixing missing error

--- omics/cli/aho.py
+++ omics/cli/aho.py
@@ -7,19 +7,24 @@ Usage:
 """
 
 import sys
-from docopt import docopt
+from docopt import docopt, DocoptExit
 
 def main():
-    args = docopt(__doc__, argv=sys.argv[1:2])
-    command = sys.argv[1] if len(sys.argv) > 1 else None
-    sub_args = sys.argv[2:]
-    if command == "run_analyzer":
-        from omics.cli.run_analyzer.__main__ import main as run_analyzer_main
-        run_analyzer_main(sub_args)
-    elif command == "rerun":
-        from omics.cli.rerun.__main__ import main as rerun_main
-        rerun_main(sub_args)
-    else:
+    try:
+        args = docopt(__doc__, argv=sys.argv[1:2])
+        command = sys.argv[1] if len(sys.argv) > 1 else None
+        sub_args = sys.argv[2:]
+        if command == "run_analyzer":
+            from omics.cli.run_analyzer.__main__ import main as run_analyzer_main
+            run_analyzer_main(sub_args)
+        elif command == "rerun":
+            from omics.cli.rerun.__main__ import main as rerun_main
+            rerun_main(sub_args)
+        else:
+            print("Unknown or missing command.")
+            print(__doc__)
+            sys.exit(1)
+    except DocoptExit:
         print("Unknown or missing command.")
         print(__doc__)
         sys.exit(1)
